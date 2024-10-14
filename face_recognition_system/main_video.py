904ca667ed2c9e6baecac1e6a2eb435e9b977fa0
ZikrullaRaxmatov <zikrulloraxmatov98@gmail.com>
2024-10-14 21:17:36 +0900

    #

--- face_recognition_system/main_video.py
+++ face_recognition_system/main_video.py
@@ -1,12 +1,23 @@
 import cv2
+import requests
+from datetime import datetime
 from simple_facerec import SimpleFacerec
+
+TOKEN = '8136431001:AAFj_QR82MMDwy9Pdke0uirE_4T6C-6tqtI'
+CHAT_ID = '755982207'
+
+def sendMessage(msg):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={msg}"
+    r = requests.get(url)
+
+student_names = ['Unknown']
 
 # Encode faces from a folder
 sfr = SimpleFacerec()
-sfr.load_encoding_images("./face/images/")
+sfr.load_encoding_images("./face_recognition_system/images/")
 
 # Load Camera
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(0)
 
 
 while True:
@@ -20,7 +31,14 @@ while True:
         cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 200, 0), 2)
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 200, 0), 4)
 
-    cv2.imshow("Frame", frame)
+        current_datetime = datetime.now()
+
+        if name not in student_names:
+            student_names.append(name)
+            sendMessage(f"{name}, {current_datetime}")
+            
+
+    cv2.imshow("Student attendance system", frame)
 
     key = cv2.waitKey(1)
     if key == 27:
