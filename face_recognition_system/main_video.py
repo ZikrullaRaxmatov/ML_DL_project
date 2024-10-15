2bdda442cb965e1bc6160f9e01dcfe731e689f5e
ZikrullaRaxmatov <zikrulloraxmatov98@gmail.com>
2024-10-15 10:57:54 +0900

    #

--- face_recognition_system/main_video.py
+++ face_recognition_system/main_video.py
@@ -3,11 +3,21 @@ import requests
 from datetime import datetime
 from simple_facerec import SimpleFacerec
 
-TOKEN = '8136431001:AAFj_QR82MMDwy9Pdke0uirE_4T6C-6tqtI'
+TOKEN = ''
 CHAT_ID = '755982207'
+students = []
 
-def sendMessage(msg):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={msg}"
+profMuhammadAftab = {
+    'TOKEN' : '7853061299:AAHRa1YTkxENch8-Bzcrjuui7J-cxBND13I',
+    'students' : ['Zikrulla Rakhmatov', 'Tolibjon']
+}
+profJaydipMehta = {
+    'TOKEN' : '8136431001:AAFj_QR82MMDwy9Pdke0uirE_4T6C-6tqtI',
+    'students' : ['Rakhmonjon', 'Adhambek']
+}
+
+def sendMessage(token, msg):
+    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={CHAT_ID}&text={msg}"
     r = requests.get(url)
 
 student_names = ['Unknown']
@@ -27,16 +37,26 @@ while True:
     face_locations, face_names = sfr.detect_known_faces(frame)
     for face_loc, name in zip(face_locations, face_names):
         y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
-
+        
         cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 200, 0), 2)
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 200, 0), 4)
 
         current_datetime = datetime.now()
+        fr = '2024-10-15 10:00'
+        to = '2024-10-15 11:30'
 
-        if name not in student_names:
-            student_names.append(name)
-            sendMessage(f"{name}, {current_datetime}")
-            
+        if  fr <= str(current_datetime) <= to:
+            match name:
+                case name if name in profMuhammadAftab['students']:
+                    if name not in student_names:
+                        student_names.append(name)
+                        sendMessage(profMuhammadAftab['TOKEN'], f"{name}, {current_datetime}")
+                        break
+                case name if name in profJaydipMehta['students']:
+                    if name not in student_names:
+                        student_names.append(name)
+                        sendMessage(profJaydipMehta['TOKEN'], f"{name}, {current_datetime}")
+                        break
 
     cv2.imshow("Student attendance system", frame)
 
--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print('Hello')
+from datetime import datetime
+
+TOKEN = ''
+students = []
+name = 'Zikrulla Rakhmatov'
+
+profMuhammadAftab = {
+    "TOKEN" : "7853061299:AAHRa1YTkxENch8-Bzcrjuui7J-cxBND13I",
+    "students" : ['Zikrulla Rakhmatov', 'Tolibjon']
+}
+profJaydipMehta = {
+    'TOKEN' : '8136431001:AAFj_QR82MMDwy9Pdke0uirE_4T6C-6tqtI',
+    'students' : ['Rakhmonjon', 'Adhambek']
+}
+
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+print(name in profMuhammadAftab["students"])
+
+def sendMessage(token, msg):
+    print(token, msg)
+
+
+match name:
+    case name if name in profMuhammadAftab["students"]:
+        sendMessage(profMuhammadAftab["TOKEN"], name)
+    case name if name in profJaydipMehta["students"]:
+        sendMessage(profJaydipMehta["TOKEN"], name)
+
+
+
+
