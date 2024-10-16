f9a0a2de9b3a8885a1da603839d76196f86e8071
ZikrullaRaxmatov <zikrulloraxmatov98@gmail.com>
2024-10-16 17:31:10 +0900

    #

--- face_recognition_system/gen_data.py
+++ face_recognition_system/gen_data.py
@@ -3,11 +3,13 @@ import os
 
 cap = cv2.VideoCapture(2)
 
+'''
 try:
-    if os.path.exists('./generated_data'):
-        os.mkdir('./generated_data')
+    if os.path.exists('../face_recognition_system/generated_data'):
+        os.mkdir('../face_recognition_system/generated_data')
 except OSError:
     print('Error: Creating directory of data')
+'''
 
 currentFrame = 0
 
@@ -15,19 +17,20 @@ while True:
     
     ret, frame = cap.read()
     
-    cv2.imshow('Showing ...', frame)
 
     if ret:
-        name = './generated_data/frame' + str(currentFrame) + '.jpg'
+        name = 'frame' + str(currentFrame) + '.jpg'
 
         print('Creating... ' + name)
 
-        cv2.imwrite(name, frame)
+        cv2.imwrite(f'./generated_data/{name}', frame)
 
         currentFrame += 1
     
     else:
         break
+
+    cv2.imshow('Showing ...', frame)
 
     if cv2.waitKey(1) == 27:
         break
