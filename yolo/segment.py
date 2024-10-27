94395ec6860b0b5a168b155c162d09c8afc599fa
ZikrullaRaxmatov <zikrulloraxmatov98@gmail.com>
2024-10-27 17:42:15 +0900

    #

--- yolo/segment.py
+++ yolo/segment.py
@@ -1,47 +1,50 @@
-import os 
-import cv2
 from ultralytics import YOLO
 from ultralytics.utils.plotting import Annotator, colors
+import os 
+import cv2
 
-model = YOLO('./yolo/weights/yolov8m-seg.pt')
+#creating model 
+model = YOLO('weights/yolov8m-seg.pt')
 names = model.model.names
 
-input_video_path = './yolo/videos/security_camera.mp4'
-output_video_path = f"./yolo/out/segment_{os.path.basename(input_video_path)}"
+# input and output path
+input_video_path = 'videos/one_way_traffic.mp4'
+output_video_path = f"out/segment_{os.path.basename(input_video_path)}"
 
-w, h = 500, 700
+#read video
 cap = cv2.VideoCapture(input_video_path)
+w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 out = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'mp4v'), 30, (w, h))
 
 while True:
-    ret, im0 = cap.read()
-    if not ret:
-        print('Video frame is empty or video processing has been completed sucseccfully.')
-        break
-    im0 = cv2.resize(im0, (w, h))
+  ret, img = cap.read()
 
-    result = model.predict(im0)
-    clss = result[0].boxes.cls.cpu().tolist()
-    masks = result[0].masks.xy
+  if not ret:
+    print('Video yakunlandi!!!')
+    break
+  
+  img = cv2.resize(img, (w, h))
 
-    annotator = Annotator(im0, line_width=2)
+  results = model.predict(img)
+  clss = results[0].boxes.cls.cpu().tolist()
+  masks = results[0].masks.xy
 
-    for mask, cls in zip(masks, clss):
-        if names[int(cls) in ['person', 'car']]:
-            try:
-                annotator.seg_bbox(mask=mask, 
-                                   mask_color=colors(int(cls), 
-                                   True), det_label=names[int(cls)])
+  annotator = Annotator(img, line_width=2)
 
-            except: continue
+  for  mask, cls in zip(masks, clss):
+    if names[int(cls) in ['car', 'track']]:
+      try:
+        annotator.seg_bbox(mask=mask, mask_color=colors, label=names[int(cls)])
+      except: continue
 
-    out.write(im0)
-    cv2.imshow('instance_segmentation', im0)
+  out.write(img)
+  #cv2.imshow('instance-segmentation', img)
 
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+  
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-out.release()
 cap.release()
+out.release()
 cv2.destroyAllWindows()
