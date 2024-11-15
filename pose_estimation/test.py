2b84b52fb1dfad9d3030b19f3ed3952f27a2baf1
ZikrullaRaxmatov <zikrulloraxmatov98@gmail.com>
2024-11-15 20:49:15 +0900

    #

--- pose_estimation/test.py
+++ pose_estimation/test.py
@@ -1,8 +1,23 @@
 import mediapipe as mp
 import cv2
 
-frame = cv2.imread('../pose_estimation/poseVideos/qolga_tayangan_holati_org.jpg')
+mpDraw = mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
 
-cv2.imshow('Imaage', frame)
+frame_path = './poseVideos/qolga_tayangan_holati_org.jpg'
 
-cv2.waitKey(1)
+frame = cv2.imread(frame_path)
+
+frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+results = pose.process(frameRGB)
+
+if results.pose_landmarks:
+  mpDraw.draw_landmarks(frame, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+  for id, lm in enumerate(results.pose_landmarks.landmark):
+    h, w, c = frame.shape
+    cx, cy = int(lm.x*w), int(lm.y*h)
+    
+    cv2.circle(frame, (cx, cy), 3, (255, 0, 0), 2)
+
+cv2.imwrite('./poseVideos/edited.jpg', frame)
